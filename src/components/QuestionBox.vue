049633994eed3eb015a1bd98f83bc76fb89f651a
samashtar <sam.ashtar@flatironschool.com>
2019-04-19 10:21:57 -0400

    correct question and counter

--- src/components/QuestionBox.vue
+++ src/components/QuestionBox.vue
@@ -10,12 +10,16 @@
           v-for="(answer,index) in answers"
           :key="index"
           @click="selectAnswer(index)"
-          :class="[selectedIndex === index ? 'selected' : '']"
+          :class="answerClass(index)"
         >{{answer}}</b-list-group-item>
       </b-list-group>
 
-      <b-button variant="primary" href="#">Submit</b-button>
-      <b-button @click="next" variant="success" href="#">Next</b-button>
+      <b-button
+        variant="primary"
+        @click="submitAnswer"
+        :disabled="selectedIndex === null || answered"
+      >Submit</b-button>
+      <b-button @click="next" variant="success">Next</b-button>
     </b-jumbotron>
   </div>
 </template>
@@ -25,11 +29,13 @@
 import _ from "lodash";
 
 export default {
-  props: { currentQuestion: Object, next: Function },
+  props: { currentQuestion: Object, next: Function, increment: Function },
   data() {
     return {
       selectedIndex: null,
-      shuffledAnswers: []
+      correctIndex: null,
+      shuffledAnswers: [],
+      answered: false
     };
   },
   computed: {
@@ -44,6 +50,7 @@ export default {
       immediate: true,
       handler() {
         this.selectedIndex = null;
+        this.answered = false;
         this.shuffleAnswers();
       }
     }
@@ -58,6 +65,34 @@ export default {
         this.currentQuestion.correct_answer
       ];
       this.shuffledAnswers = _.shuffle(answers);
+      this.correctIndex = this.shuffledAnswers.indexOf(
+        this.currentQuestion.correct_answer
+      );
+    },
+    submitAnswer() {
+      let isCorrect = false;
+
+      if (this.selectedIndex === this.correctIndex) {
+        isCorrect = true;
+      }
+      this.answered = true;
+      this.increment(isCorrect);
+    },
+    answerClass(index) {
+      let answerClass = "";
+
+      if (!this.answered && this.selectedIndex === index) {
+        answerClass = "selected";
+      } else if (this.answered && this.correctIndex === index) {
+        answerClass = "correct";
+      } else if (
+        this.answered &&
+        this.selectedIndex === index &&
+        this.correctIndex !== index
+      ) {
+        answerClass = "incorrect";
+      }
+      return answerClass;
     }
   }
 };
