eae5ec85735978b8a99d5ddc3d91e10dec770683
samashtar <sam.ashtar@flatironschool.com>
2019-04-19 09:46:45 -0400

    shuffle answers, css and styling

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
     <b-container class="bv-example-row">
       <b-row>
         <b-col sm="6" offset="3">
-          <QuestionBox :currentQuestion="questions[index]"></QuestionBox>
+          <QuestionBox v-if="questions.length" :currentQuestion="questions[index]" :next="next"></QuestionBox>
         </b-col>
       </b-row>
     </b-container>
@@ -26,6 +26,11 @@ export default {
       questions: [],
       index: 0
     };
+  },
+  methods: {
+    next() {
+      this.index++;
+    }
   },
   mounted: function() {
     fetch("https://opentdb.com/api.php?amount=10&category=27&type=multiple", {
